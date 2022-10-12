8223bf98e90e2886ede6885df214e2f566e8b798
Ramai Alejandria <vyasadev931@gmail.com>
2022-10-12 09:26:35 +0200

    new implementations

--- src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,15 +1,31 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    public void beforeEachSetup () {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    public void tearDownAfterEach () {
+        System.out.println("Running @AfterEach");
+        System.out.println();
+    }
+
     @Test
     public void equalsAndNotEqualsTest () {
          System.out.println("Running Test: equalsAndNotEqualsTest");
-         DemoUtils demoUtils = new DemoUtils();
          assertEquals(6,demoUtils.add(2,4),"2+4 must be 6");
          assertNotEquals(6,demoUtils.add(1,9),"1+9 must not be 6");
     }
@@ -17,7 +33,6 @@ public class DemoUtilsTest {
     @Test
     public void nullAndNotNullTest () {
         System.out.println("Running Test: nullAndNotNullTest");
-        DemoUtils demoUtils = new DemoUtils();
         String str1 = null;
         String str2 = "ralexandria";
         assertNull(demoUtils.checkNull(str1),"Object should be null");
